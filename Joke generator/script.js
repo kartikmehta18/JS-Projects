e42c59fd9e2a0cc690e455f7ef01ebe92803520d
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-07-03 14:38:04 +0530

    joke generator

---
+++ Joke generator/script.js
@@ -0,0 +1,17 @@
+const jokeContainer =document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url ="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+let getJoke = () =>{
+    fetch(url)
+    .then(data => data.json())
+    .then(item =>jokeContainer.textContent=`${item.joke}`);
+}
+getJoke();
+
+async function getJoke1(){
+    const fe = await fetch(url);
+    const data =await fe.json();
+    jokeContainer.innerText=`${data.joke}`;
+}
+getJoke1();
