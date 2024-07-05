b287cc4cd892ebf3c69882881de837ddec3b1567
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-07-05 16:38:23 +0530

    Add jokegeneratorAPI

--- Joke generator/script.js
+++ Joke generator/script.js
@@ -1,37 +1,15 @@
-const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-const result = document.getElementById("result");
-const sound = document.getElementById("sound");
-const btn = document.getElementById("search-btn");
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
-btn.addEventListener("click", () => {
-    let inpWord = document.getElementById("inp-word").value;
-    fetch(`${url}${inpWord}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            result.innerHTML = `
-            <div class="word">
-                    <h3>${inpWord}</h3>
-                    <button onclick="playSound()">
-                        <i class="fas fa-volume-up"></i>
-                    </button>
-                </div>
-                <div class="details">
-                    <p>${data[0].meanings[0].partOfSpeech}</p>
-                    <p>/${data[0].phonetic}/</p>
-                </div>
-                <p class="word-meaning">
-                   ${data[0].meanings[0].definitions[0].definition}
-                </p>
-                <p class="word-example">
-                    ${data[0].meanings[0].definitions[0].example || ""}
-                </p>`;
-            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
-        })
-        .catch(() => {
-            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
-        });
-});
-function playSound() {
-    sound.play();
+let getJoke = () => {
+    jokeContainer.classList.remove("fade");
+    fetch(url)
+    .then(data => data.json())
+    .then(item =>{
+        jokeContainer.textContent = `${item.joke}`;
+        jokeContainer.classList.add("fade");
+    });
 }
+
+getJoke();
