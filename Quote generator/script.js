f8fe88f3d0aeaeb4ec0e565e4ede3f35bd168933
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-07-10 19:47:30 +0530

    Recipe App

--- Quote generator/script.js
+++ Quote generator/script.js
@@ -10,4 +10,22 @@ fetch(url + "pizza")
 .then((data)=> {let myMeal = data.meals[0];
 // console.log(data);
 console.log(myMeal);
+console.log(myMeal.strMeal);
+console.log(myMeal.strMealThumb);
+console.log(myMeal.strArea);
+console.log(myMeal.strInstructions);
+let count = 1;
+let ingredients = [];
+for(let i in myMeal){
+    let ingredient ="";
+    let measure ="";
+    if (i.startsWith("strIngredient") && myMeal[i]) {
+        ingredient = myMeal[i];
+        measure = myMeal[`strMeasure` + count];
+        count += 1;
+        ingredients.push(`${measure} ${ingredient}`);
+    }
+}
+console.log(ingredients);
+      
 });
