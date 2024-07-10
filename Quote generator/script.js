623ad12f78c0feb61d036cd2ec504e35fa0e1de5
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-07-10 18:44:43 +0530

    Recipe App

--- Quote generator/script.js
+++ Quote generator/script.js
@@ -1,4 +1,13 @@
 let result = document.getElementById("result");
 let btn = document.getElementById("btn");
 
-let url = ""
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+let userInp = document.getElementById("inp").value;
+
+fetch(url + "pizza")
+.then(response => response.json())
+.then((data)=> {let myMeal = data.meals[0];
+// console.log(data);
+console.log(myMeal);
+});
